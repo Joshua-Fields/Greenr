bdfa4eab5e47ea2938ac0727a81cefbec3845706
Selina <mariiaselina@gmail.com>
2020-12-16 15:56:42 +0200

    Spring Boot project initialised

---
+++ greenr/greenr/src/main/java/com/company/greenr/GreenrApplication.java
@@ -0,0 +1,13 @@
+package com.company.greenr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenrApplication.class, args);
+	}
+
+}
